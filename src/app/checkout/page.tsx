a9aaef6382c0654c39c76d706582a1b970005fe4
Chris Demetriad <chris@demetriad.co.uk>
2026-04-19 19:15:36 +0100

    adding checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import styles from "./page.module.css";
+
+const CheckoutPage = () => {
+	return <main className={styles.main}>checkout page</main>;
+};
+
+export default CheckoutPage;
